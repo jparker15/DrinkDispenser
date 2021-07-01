79885eb7a6e21fb95695f2fb7ca2fa88c6def0c1
Jeremy <jeremyeparker@yahoo.com>
2021-07-01 19:29:31 -0400

    set shot fields, dispense method, find total volume method

--- src/com/company/Shots.java
+++ src/com/company/Shots.java
@@ -1,2 +1,26 @@
-package com.company;public class Shots {
+package com.company;
+
+public class Shots {
+
+    private String flavor;
+    private int totalAmount;
+    private int shotAmount;
+
+    public Shots(String flavor, int totalAmount, int shotAmount){
+        this.flavor = flavor;
+        this.totalAmount = totalAmount;
+        this.shotAmount = shotAmount;
+    }
+
+    public String dispense(){
+        if(totalAmount < shotAmount){
+            return null;
+        }
+        totalAmount -= shotAmount;
+        return flavor;
+    }
+
+    public void getVolume(){
+        System.out.println("total number of shots left:" + totalAmount/shotAmount);
+    }
 }
