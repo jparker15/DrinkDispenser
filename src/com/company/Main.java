cd2dbaf48428d67713d983a30a9e2deb6752ac53
Jeremy <jeremyeparker@yahoo.com>
2021-07-01 19:30:50 -0400

    instructions

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        /*
+    DrinkDispenser system will have a collection of Syrups.
+    These syrups will have a name and a volume a collection of Shots.
+    These will have a Flavor a volume and amount to dispense per shot.
+    Three different CupHolders with sizes 'small' 'medium' large' (you can decide what those numbers are)*/
+
+        System.out.println(100/10);
+    }
+}
