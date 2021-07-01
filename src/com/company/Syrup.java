ea2e6e1e94621b5e53982582304bd724f9e07877
Jeremy <jeremyeparker@yahoo.com>
2021-07-01 19:30:31 -0400

    set name, set set/modify totalAmt

--- src/com/company/Syrup.java
+++ src/com/company/Syrup.java
@@ -1,2 +1,22 @@
-package com.company;public class Syrup {
+package com.company;
+
+public class Syrup {
+
+    private String drinkName;
+    private int totalAmount;
+
+
+    public Syrup (String drinkName, int totalAmount){
+        this.drinkName = drinkName;
+        this.totalAmount = totalAmount;
+
+    }
+
+    public void dispense(int amt){
+        totalAmount -= amt;
+    }
+
+    public int getLiquidTotal(){
+        return totalAmount;
+    }
 }
