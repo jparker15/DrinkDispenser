adac4f09e15b1eb8f2d9f77c6e1722e063d5074f
Jeremy <jeremyeparker@yahoo.com>
2021-07-01 19:27:46 -0400

    parent class

--- src/com/company/DrinkDispenser.java
+++ src/com/company/DrinkDispenser.java
@@ -1,2 +1,19 @@
-package com.company;public class DrinkDispenser {
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DrinkDispenser {
+
+    List syrups = new ArrayList<Syrup>();
+    List shots = new ArrayList<Shots>();
+
+    CupHolder small = new CupHolder(8,150);
+    CupHolder medium = new CupHolder(16,200);
+    CupHolder large = new CupHolder(32,150);
+
+
+    public void serveDrink(char size, int syrupIndex){
+
+    }
 }
